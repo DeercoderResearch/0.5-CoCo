e7a835e691dd931508ff75ab525263705a07a010
Liu Chang <lllccc2009@gmail.com>
2015-03-03 08:07:18 -0500

    Update some files

--- PythonAPI/mycode.py
+++ PythonAPI/mycode.py
@@ -1,7 +1,10 @@
 from pycocotools.coco import COCO
+from write_xml import write_to_file
 import numpy as np
 import skimage.io as io
 import matplotlib.pyplot as plt
+import os
+import shutil
 
 dataDir='..'
 dataType='val2014'
@@ -23,6 +26,33 @@ for cat in cats:
 foodCategoryId = coco.getCatIds(foodCategory)
 foodImageId = coco.getImgIds(catIds=foodCategoryId) #must add catIds=
 
+dstdir = './JPEGImages/'
+
+# Get all food images and copy them to JPEGImages folders.(#JPEGImage#)
+for cat in range(0, len(foodImageId)):
+	img = coco.loadImgs(foodImageId[cat])[0]
+	img_name = '%s/images/%s/%s'%(dataDir,dataType,img['file_name'])
+	#print img_name
+	shutil.copy(img_name, dstdir)
+
+
+# Generate the SegmentationObject/SegmentationClass (#Segmentation#)
+dstdir = './SegmentationClass'
+dstdir_2 = './SegmentationObject'
+
+# Generate the configuration files for Annotation folders(#Annotation#)
+# Move to the above the share the loop of image_names.
+for cat in range(0, len(foodImageId)):
+	img = coco.loadImgs(foodImageId[cat])[0]
+	img_name = os.path.splitext(img['file_name'])[0]
+	img_annotation_xml_name ='./Annotations/%s.xml'%(img_name)
+	print img_annotation_xml_name
+	file = open(img_annotation_xml_name, "wb")
+	file.close()
+
+### ?????? 
+
+# Generat the configuration for ImageSet
 
 
 img = coco.loadImgs(foodImageId[5])[0]
@@ -32,6 +62,7 @@ plt.figure()
 plt.imshow(I)
 plt.show()
 
+# JUST FOR DEBUGGING
 print foodCategory
 print foodCategoryId	
 print foodImageId
