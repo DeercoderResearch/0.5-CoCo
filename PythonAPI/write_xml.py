b1450b58bc50567dcc1390f805e1013388084102
Liu Chang <lllccc2009@gmail.com>
2015-04-21 23:50:07 -0400

    remove the print of string

--- PythonAPI/write_xml.py
+++ PythonAPI/write_xml.py
@@ -75,14 +75,14 @@ def write_to_file(img_name,food_type, file_name, img_width, img_height,left_x, l
 	object.appendChild(bndbox)
 	annotation.appendChild(object)
 	 
-	print doc.toprettyxml() 
+	#print doc.toprettyxml() 
 
 	## add last processing to avoid the <?xml=xxxx> area
 	lines = doc.toprettyxml()
 	firstlineEndIndex = lines.find(">")
-	print firstlineEndIndex
+#	print firstlineEndIndex
 	new_file_content = lines[firstlineEndIndex+2:]
-	print new_file_content
+#	print new_file_content
 
 	# write to xml file
 	f = open(file_name, "w")
